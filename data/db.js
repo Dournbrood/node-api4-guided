2dec8925c703db4db7377b9e47908ac7aa8e961b
Dournbrood <tanner1220002@gmail.com>
2020-01-16 13:23:10 -0500

    asdf

--- data/db.js
+++ data/db.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 
 const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile.js')[environment];
+const config = require('../knexfile')[environment];
+console.log(config);
 
 module.exports = knex(config);
